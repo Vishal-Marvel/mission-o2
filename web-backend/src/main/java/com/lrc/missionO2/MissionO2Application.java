f19e3a7c203394b0f81c773671c305ba94207020
Vishal-Marvel <vishal.pvn.edu@gmail.com>
2023-08-27 08:38:20 +0530

    configured cors

--- web-backend/src/main/java/com/lrc/missionO2/MissionO2Application.java
+++ web-backend/src/main/java/com/lrc/missionO2/MissionO2Application.java
@@ -2,8 +2,10 @@ package com.lrc.missionO2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@CrossOrigin
 public class MissionO2Application {
 
 	public static void main(String[] args) {
---
+++ web-backend/src/main/java/com/lrc/missionO2/config/CorsConfig.java
@@ -0,0 +1,28 @@
+package com.lrc.missionO2.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * A configuration class for Cross-Origin Resource Sharing (CORS).
+ * This class defines the CORS filter bean that allows requests from a specific origin,
+ * with any method and header, and supports credentials.
+ */
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("*"); // Replace with your frontend URL
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+//        config.setAllowCredentials(true);
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
